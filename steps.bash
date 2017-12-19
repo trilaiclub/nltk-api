08b1c9ed0550528d155bdfb994ae67a99f26839a
trilaiclub <trilaiclub@gmail.com>
2017-12-20 00:07:02 +0530

    Environment Issue.

---
+++ steps.bash
@@ -0,0 +1,14 @@
+sudo pip install -U nltk
+sudo python -m nltk.downloader maxent_treebank_pos_tagger
+sudo python -m nltk.downloader punkt
+sudo python -m nltk.downloader averaged_perceptron_tagger
+sudo python -m nltk.downloader nltk_stopwords
+sudo python -m nltk.downloader stopwords
+sudo python -m nltk.downloader wordnet
+
+sudo yum install git
+git clone https://github.com/trilaiclub/nltk-api.git
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo yum -y install nodejs
+npm install loopback
+
