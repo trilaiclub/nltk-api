4d50bfeb746e6a950b55592509d6c17fe0134484
trilaiclub <trilaiclub@gmail.com>
2018-01-02 10:15:32 +0530

    New changes with grammar

--- steps.bash
+++ steps.bash
@@ -5,12 +5,13 @@ sudo python -m nltk.downloader punkt -d ~/nltk_data
 sudo python -m nltk.downloader averaged_perceptron_tagger -d ~/nltk_data
 sudo python -m nltk.downloader stopwords -d ~/nltk_data
 sudo python -m nltk.downloader wordnet -d ~/nltk_data
+sudo python -m nltk.downloader tagsets -d ~/nltk_data
 
 sudo yum install git
 git clone https://github.com/trilaiclub/nltk-api.git
 curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo yum -y install nodejs
 npm install loopback
-npm install -g forever
+sudo npm install -g forever
 forever start --verbose --append --uid \"nltk\" --watch --watchDirectory ./server server/server.js --spinSleepTime 1000ms
 
